a60056dd05c3865808bf4bd6b594b5ee955022b7
space-mOa <space-moa@seznam.cz>
2020-04-05 00:41:32 +0200

    /

--- src/getData/main.go
+++ src/getData/main.go
@@ -40,6 +40,7 @@ func checkCorrectness(n Node, checkN Node) {
 		for iV, v := range checkN.values[i].line {
 			if v != n.values[i].line[iV] {
 				fmt.Println("error")
+				fmt.Println("check:\n", v, n.values[i].line[iV], checkN.values[i].line, n.values[i].line)
 				panic("PANIC")
 			}
 		}
@@ -53,13 +54,48 @@ func checkCorrectness(n Node, checkN Node) {
 	}
 }
 
+func sociologists(page Page) []string {
+	rgx := getRegexp(
+		`<span class="PERSON_BORN"><time datetime=.*>.*</time>.*</span>`, // 0 Vybere datum narození
+		`<span class="PERSON_DIED"><time datetime=.*>.*</time>.*</span>`, // 1 vybere datum úmrtí
+		`<span class="PERSON_DIED">\?\?\?</span>`,                        // 2 Vybere neznámé datum umrtí
+		`datetime=".*"`, // 3 Vyber časový údaj s datetime
+		`[[:digit:]-]*`, // 4 Vyber číslice
+		`<span class="PERSON_DIED"><time datetime=.*>.*</time>.*<tim`, // 5 vybere datum úmrtí
+	)
+	text := string(page.Revision.Text)
+	var born string
+	for _, ch := range rgx[4].FindAllString(rgx[3].FindAllString(rgx[0].FindAllString(text, -1)[0], -1)[0], -1) {
+		born = born + ch
+	}
+	var died string
+	if s := rgx[1].FindAllString(text, -1); s == nil {
+		died = "2030"
+		if rgx[2].FindAllString(text, -1) != nil {
+			died = "0000"
+		}
+	} else {
+		d := rgx[1].FindAllString(text, -1)[0]
+		if d1 := rgx[5].FindAllString(d, -1); d1 != nil {
+			for _, ch := range rgx[4].FindAllString(rgx[3].FindAllString(d1[0], -1)[0], -1) {
+				died = died + ch
+			}
+		} else {
+			for _, ch := range rgx[4].FindAllString(rgx[3].FindAllString(d, -1)[0], -1) {
+				died = died + ch
+			}
+		}
+	}
+	return []string{string(page.Title), born, died}
+}
+
 func main() {
 	data := &WikiData{}
 	err := unpackFile(data, "./dump.xml")
 	if err != nil {
 		fmt.Println(err)
 	}
-	soc := data.getSCSg()
+	sociologists := data.getCategoryModify("Sociologove", []string{"ID", "jmeno", "narozeni", "umrti"}, `\[\[Kategorie:SCSg.*\]\]`, sociologists)
 	Sizc := data.getCategory("Sizc", []string{"ID", "heslo"}, `(\[\[Kategorie:SIZCSg.*\]\])`)
 	MalySgS := data.getCategory("MalySgS", []string{"ID", "heslo"}, `\[\[Kategorie:MSgS.*\]\]`)
 	VelkySgS := data.getCategory("VelkySgS", []string{"ID", "heslo"}, `\[\[Kategorie:VSgS.*\]\]`)
@@ -86,7 +122,6 @@ func main() {
 		}
 		return nil
 	})
-	saveNodes("./data/nodes/", soc, MalySgS, Sizc, journals, institutions, VelkySgS)
-	makeAndSaveEdges("./data/edges/", soc, MalySgS, Sizc, journals, institutions, VelkySgS)
-
+	saveNodes("./data/nodes/", sociologists, MalySgS, Sizc, journals, institutions, VelkySgS)
+	makeAndSaveEdges("./data/edges/", sociologists, MalySgS, Sizc, journals, institutions, VelkySgS)
 }
