1c1c9235536f02d601c9357fcf6e569928f7dafe
space-mOa <space-moa@seznam.cz>
2021-04-27 19:04:07 +0200

    VSgS strom

---
+++ src/2/data/VSgS/m.go
@@ -0,0 +1,19 @@
+[id id1 id2 name1 name2]
+[1 38 125 Analýza Sedláček Oto]
+[2 321 21 Demografie Fajfr František]
+[3 321 76 Demografie Kučera Milan]
+[4 321 97 Demografie Možný Ivo]
+[5 321 106 Demografie Pavlík Zdeněk]
+[6 321 135 Demografie Srb Vladimír]
+[7 321 166 Demografie Večerník Jiří]
+[8 1556 20 Parlament Dušek Jan]
+[9 1556 85 Parlament Machotka Otakar]
+[10 1556 94 Parlament Mertl Jan]
+[11 1556 134 Parlament Sorokin Pitirim Alexandrovič]
+[12 1556 167 Parlament Verunáč Václav]
+[13 2548 28 Teorie vědy Frič Pavol]
+
+
+514, 527
+
+524
